c586e59314138eb0ecfccfcd51121cc45f384af0
r2newell <42938289+r2newell@users.noreply.github.com>
2018-12-18 13:34:14 -0500

    Add files via upload

---
+++ binarySearch.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "utility.h"
+
+
+
+int binarySearch(const int *seq, int key, int size)
+{
+	int start = 0; 
+	int end = size; 
+	int middle; 
+
+	while(start < end)
+	{
+	
+	    printf("\n%d\n", middle);
+		middle = (start + end) / 2; 
+		if(seq[middle] == key)
+			   return middle; 
+		else if(key < seq[middle])
+			  end = middle; 
+		else 
+			   start = middle + 1; 
+
+	}
+
+	return 0; 
+
+}
+
